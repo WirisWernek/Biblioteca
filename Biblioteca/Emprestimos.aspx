0dbaa84b5eff33b321e7c3ac7c465e24beffac13
Wiris Rafael <wiris.wernek@itixti.com.br>
2023-05-06 13:55:05 -0300

    Add tela de emprestimos

---
+++ Biblioteca/Emprestimos.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Emprestimos.aspx.cs" Inherits="Biblioteca.Emprestimos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <asp:Label ID="lblDpi" runat="server" Text="DPI do Usuario"></asp:Label>            
+		<asp:TextBox ID="dpiInput" runat="server"></asp:TextBox><br/>
+        <asp:Label ID="lblIsbn" runat="server" Text="ISBN do Livro"></asp:Label>
+        <asp:TextBox ID="isbnInput" runat="server"></asp:TextBox><br />
+        <asp:Button ID="btnEmprestar" runat="server" Text="Emprestar"/><br />
+    </div>
+        <asp:Label ID="lblAviso" runat="server" Text="" Visible="False"></asp:Label>
+</asp:Content>
---
+++ Biblioteca/Emprestimos.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Biblioteca
+{
+
+
+    public partial class Emprestimos
+    {
+
+        /// <summary>
+        /// Controle lblDpi.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDpi;
+
+        /// <summary>
+        /// Controle dpiInput.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox dpiInput;
+
+        /// <summary>
+        /// Controle lblIsbn.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblIsbn;
+
+        /// <summary>
+        /// Controle isbnInput.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox isbnInput;
+
+        /// <summary>
+        /// Controle btnEmprestar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEmprestar;
+
+        /// <summary>
+        /// Controle lblAviso.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAviso;
+    }
+}
